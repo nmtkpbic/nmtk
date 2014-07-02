c67b35cc5e2e21d291f4c12f4cd0ef532e242794
Chander Ganesan <chander@otg-nc.com>
2014-07-02 21:02:04 +0000

    Add license text to all the NMTK code components

--- NMTK_apps/NMTK_ui/static/NMTK_ui/js/nmtk_ui.js
+++ NMTK_apps/NMTK_ui/static/NMTK_ui/js/nmtk_ui.js
@@ -1,3 +1,34 @@
+/**
+ * @license Non-Motorized Toolkit
+ * Copyright (c) 2014 Open Technology Group Inc. (A North Carolina Corporation)
+ * Developed under Federal Highway Administration (FHWA) Contracts:
+ * DTFH61-12-P-00147 and DTFH61-14-P-00108
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, 
+ * are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright notice, 
+ *       this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright 
+ *       notice, this list of conditions and the following disclaimer 
+ *       in the documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Open Technology Group, the name of the 
+ *       Federal Highway Administration (FHWA), nor the names of any 
+ *       other contributors may be used to endorse or promote products 
+ *       derived from this software without specific prior written permission.
+ *       
+ *       THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+ *       "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+ *       LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+ *       FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL 
+ *       Open Technology Group Inc BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+ *       SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+ *       LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF 
+ *       USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED 
+ *       AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+ *       OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT 
+ *       OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
+ *       SUCH DAMAGE.
+ */
 "use strict";
 require.config({
 	paths: {
--- reset.sh
+++ reset.sh
@@ -1,11 +1,60 @@
 #!/bin/bash
+# Non-Motorized Toolkit
+# Copyright (c) 2014 Open Technology Group Inc. (A North Carolina Corporation)
+# Developed under Federal Highway Administration (FHWA) Contracts:
+# DTFH61-12-P-00147 and DTFH61-14-P-00108
+# 
+# Redistribution and use in source and binary forms, with or without modification, 
+# are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright notice, 
+#       this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright 
+#       notice, this list of conditions and the following disclaimer 
+#       in the documentation and/or other materials provided with the distribution.
+#     * Neither the name of the Open Technology Group, the name of the 
+#       Federal Highway Administration (FHWA), nor the names of any 
+#       other contributors may be used to endorse or promote products 
+#       derived from this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL 
+# Open Technology Group Inc BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF 
+# USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED 
+# AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT 
+# OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 # Chander's script to reset his NMTK environment for testing.
 # First stop celery
-USERNAME=chander
-EMAIL=chander@otg-nc.com
-PASSWORD=jraw123
-FIRSTNAME="Chander"
-LASTNAME="Ganesan"
+pushd $(dirname $0)
+if [ -f ./config ]; then
+  source ./config
+fi
+if [ ${#USERNAME} == 0 ]; then
+  echo -n "Username: " 
+  read USERNAME
+fi
+if [ ${#EMAIL} == 0 ]; then
+  echo -n "Email Address: "
+  read EMAIL
+fi
+if [ ${#PASSWORD} == 0 ]; then
+  echo -n "Password: "
+  read -s PASSWORD
+fi
+if [ ${#FIRSTNAME} == 0 ]; then
+  echo -n "Password: "
+  read -s FIRSTNAME
+fi
+if [ ${#LASTNAME} == 0 ]; then
+  echo -n "Password: "
+  read -s LASTNAME
+fi
+export FIRSTNAME LASTNAME PASSWORD EMAIL USERNAME
+
 BASEDIR=$(dirname $0)
 CELERYD_NAME=$(hostname -s)
 if [[ -f /var/run/celery/$CELERYD_NAME.pid ]]; then
@@ -38,5 +87,6 @@ popd
 
 sudo /etc/init.d/apache2 restart
 sudo /etc/init.d/celeryd-$CELERYD_NAME start
+popd
 
 
