8704701d65cb667f6f151e18513d38cd6857e72d
Chander Ganesan <chander@otg-nc.com>
2015-01-08 17:19:05 -0500

    Reorder the applications so that NMTK_Server (which overrides the template from the admin app) is first.  This addresses ticket #51

--- NMTK_apps/NMTK_apps/settings.py
+++ NMTK_apps/NMTK_apps/settings.py
@@ -174,11 +174,11 @@ if TOOL_SERVER:
     INSTALLED_APPS = INSTALLED_APPS + ('NMTK_tools', # An app used to generate a list of tools.
                                        ) + NMTK_TOOL_APPS
 if NMTK_SERVER:
-    INSTALLED_APPS = INSTALLED_APPS + ('django.contrib.auth',        
-                                       'django.contrib.admin',
+    INSTALLED_APPS = INSTALLED_APPS + ('django.contrib.auth',
                                        'registration',
                                        'widget_tweaks',
                                        'NMTK_server', # The NMTK Server
+                                       'django.contrib.admin',
                                        'NMTK_ui', # the UI components for NMTK server
                                        'tastypie',
                                        )
